0111d703c76136edc90a848621a2f511aa7c79f7
Rogerio Filho <100595069+rogeriofilhoO@users.noreply.github.com>
2024-05-10 18:18:52 -0400

    Sobre e header e banner

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifolio Rogerio Filho</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+
+    <script src="https://kit.fontawesome.com/19ce1ec299.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="nav-container">
+                <h1 class="nav-title"><a href="index.php">ROGERIO FILHO</a></h1>
+                <div>
+                    <a href="#home" class="nav-itens">Home</a>
+                    <a href="#sobre" class="nav-itens">Sobre</a>
+                    <a href="#trabalhos" class="nav-itens">Trabalhos</a>
+                    <a href="#contato" class="nav-itens">Contato</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="Banner" id="home">
+
+            <div class="banner-div">
+                <p>Oi sou o Rogerio</p>
+                <h1>FRONT-END</h1>
+                <a href="#sobre">SOBRE</a>
+            </div>
+            <div>
+                <img src="assets/img/Group 1barras.png" alt="Barras" class="bannerimg">
+            </div>
+
+        </section>
+
+        <section class="sobre" id="sobre">
+            <h1>ROGERIO RODRIGUES BENEVIDES FILHO</h1>
+
+            <div class="sobre-div">
+                <p>Descrição</p>
+
+                <img src="assets/img/Group 2Quadrados.png" alt="Quadrados roxos" width="500" height="500">
+            </div>
+            
+        </section>
+
+        <section class="trabalhos" id="trabalhos">
+
+        </section>
+    </main>
+    <footer>
+        <div id="contato" class="footer-container">
+            <div class="footer-icons">
+                <i class="fa-brands fa-instagram"><a href="#"></a></i>
+                <i class="fa-brands fa-bluesky"><a href="#"></a></i>
+                
+            </div>
+            <p>© 2024 Rogerio Filho, All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
