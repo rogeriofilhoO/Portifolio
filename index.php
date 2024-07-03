b74fb4066940201ffd0012940a0b20a5d9fef531
Rogerio Filho <100595069+rogeriofilhoO@users.noreply.github.com>
2024-07-03 18:16:22 -0400

    Nova versão

--- index.php
+++ index.php
@@ -14,53 +14,87 @@
 <body>
     <header>
         <nav>
-            <div class="nav-container">
-                <h1 class="nav-title"><a href="index.php">ROGERIO FILHO</a></h1>
-                <div>
-                    <a href="#home" class="nav-itens">Home</a>
-                    <a href="#sobre" class="nav-itens">Sobre</a>
-                    <a href="#trabalhos" class="nav-itens">Trabalhos</a>
-                    <a href="#contato" class="nav-itens">Contato</a>
+            <div class="container">
+                <div class="nav-container">
+                    <h1 class="nav-title"><a href="index.php">ROGERIO FILHO</a></h1>
+                    <div>
+                        <a href="#home" class="nav-itens">Home</a>
+                        <a href="#sobre" class="nav-itens">Sobre</a>
+                        <a href="#trabalhos" class="nav-itens">Trabalhos</a>
+                        <a href="#contato" class="nav-itens">Contato</a>
+                    </div>
                 </div>
             </div>
         </nav>
     </header>
     <main>
-        <section class="Banner crosshair" id="home">
-            <div class="banner-div link-effect-14">
-                <p>Oi sou o Rogerio</p>
-                <h1>FRONT END</h1>
-                <a href="#sobre"><span>SOBRE</span></a>
-            </div>
-            <div>
-                <img src="assets/img/Group 1barras.png" alt="Barras" class="bannerimg">
-            </div>
+        <section class="container">
+            <section class="Banner crosshair" id="home">
+                <article class="banner-div link-effect-14">
+                    <p>Oi sou o Rogerio</p>
+                    <h1>FRONT END</h1>
+                    <a href="#sobre"><span>SOBRE</span></a>
+                </article>
+                <figure>
+                    <img src="assets/img/Group 1barras.png" alt="Barras" class="bannerimg">
+                </figure>
 
+            </section>
         </section>
 
-        <section class="sobre" id="sobre">
-            <h1>ROGERIO RODRIGUES BENEVIDES FILHO</h1>
+        <section class="container">
+            <section class="sobre" id="sobre">
+                <h1>ROGERIO RODRIGUES BENEVIDES FILHO</h1>
 
-            <div class="sobre-div">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At enim autem iure distinctio. Neque aliquam dolor veritatis voluptas optio architecto distinctio dolorum quam perspiciatis laboriosam. Voluptas consequuntur quae esse obcaecati quo adipisci ratione exercitationem natus omnis delectus assumenda tenetur reiciendis modi minus, possimus accusamus distinctio similique, consectetur, soluta nisi eligendi amet minima ut? Repellat, modi aut facilis nulla autem amet accusantium eligendi nam eum, blanditiis, voluptas fuga impedit. Sunt quibusdam sit explicabo quisquam corporis provident? In doloremque deserunt, quod voluptas quos esse quasi, excepturi perferendis dolor, quas earum? Corrupti quae et illum debitis nesciunt vero velit cum ipsam explicabo repellat?</p>
+                <article class="sobre-div">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At enim autem iure distinctio. Neque aliquam dolor veritatis voluptas optio architecto distinctio dolorum quam perspiciatis laboriosam. Voluptas consequuntur quae esse obcaecati quo adipisci ratione exercitationem natus omnis delectus assumenda tenetur reiciendis modi minus, possimus accusamus distinctio similique, consectetur, soluta nisi eligendi amet minima ut? Repellat, modi aut facilis nulla autem amet accusantium eligendi nam eum, blanditiis, voluptas fuga impedit. Sunt quibusdam sit explicabo quisquam corporis provident? In doloremque deserunt, quod voluptas quos esse quasi, excepturi perferendis dolor, quas earum? Corrupti quae et illum debitis nesciunt vero velit cum ipsam explicabo repellat?</p>
 
-                <img src="assets/img/Group 2Quadrados.png" alt="Quadrados roxos" width="500" height="500">
-            </div>
-            
+                    <img src="assets/img/Group 2Quadrados.png" alt="Quadrados roxos" width="500" height="500">
+                </article>
+
+            </section>
         </section>
 
-        <section class="trabalhos" id="trabalhos">
-            <div class="trab-div">
-                
-                <div class="trab-div2">
-                    <img src="assets/img/Group 3.png" alt="barras" width="50" height="50">
-                    <h1>TRABALHOS FEITOS</h1>
+        <section class="container">
+            <section class="trabalhos" id="trabalhos">
+                <div class="trab-div">
+
+                    <div class="trab-div2">
+                        <img src="assets/img/Group 3.png" alt="barras" width="50" height="50">
+                        <h1>TRABALHOS FEITOS</h1>
+                    </div>
+
+                    <a href="">TODOS</a>
                 </div>
 
-                <a href="">TODOS</a>
-            </div>
-
-            <!-- trabalhos -->
+                <div class="projetos">
+                    <div class="proj-container">
+                        <img src="" alt="">
+                        <p>Dehumanos - Cadastro</p>
+                        <a href="" target="_blank">Ver Projeto</a>
+                    </div>
+                    <div class="proj-container">
+                        <img src="" alt="">
+                        <p>Dehumanos GFT</p>
+                        <a href="" target="_blank">Ver Projeto</a>
+                    </div>
+                    <div class="proj-container">
+                        <img src="" alt="">
+                        <p>Dehumanos</p>
+                        <a href="" target="_blank">Ver Projeto</a>
+                    </div>
+                    <div class="proj-container">
+                        <img src="" alt="">
+                        <p>Sistema Condomínio Serra Verde</p>
+                        <a href="" target="_blank">Ver Projeto</a>
+                    </div>
+                    <div class="proj-container">
+                        <img src="" alt="">
+                        <p>Urbantech</p>
+                        <a href="" target="_blank">Ver Projeto</a>
+                    </div>
+                </div>
+            </section>
         </section>
     </main>
     <footer>
